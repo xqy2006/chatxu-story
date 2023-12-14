0b0b03f473e9287ae57e6b79a37b617891270d4f
xuqinyang <2601928946@qq.com>
2023-12-14 08:08:33 +0800

    Update llama2c-worker.js

--- public/llama2c-worker.js
+++ public/llama2c-worker.js
@@ -17,9 +17,9 @@ self.addEventListener('message', async function(e){
   //var fileRequest = await fetch("https://github.com/xqy2006/chatxu-story/releases/download/0.0.1/tokenizer.bin");
   var fileContent = await fileRequest.arrayBuffer();
 
-  var modelURL = pageDirectory + '/' + 'model1.bin';
+  var modelURL = pageDirectory + '/' + 'model_1.bin';
   //var modelURL = "http://storage.live.com/items/D35C9B07337400A!5338:/model.bin?authkey=!AMqpKPn7vxsmE8c";
-  if (isLocalhost()) { modelURL = pageDirectory + '/' + 'model1.bin'; }
+  if (isLocalhost()) { modelURL = pageDirectory + '/' + 'model_1.bin'; }
 
   var modelFileRequest = await fetch(modelURL);
   var modelURL = pageDirectory + '/' + 'model_2.bin';
